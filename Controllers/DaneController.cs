506399a8ce39911bce09e0e9c641e6f471d0dd35
Menrok <sz.mi@interia.pl>
2024-11-03 10:32:20 +0100

    0.0.1

---
+++ Controllers/DaneController.cs
@@ -0,0 +1,24 @@
+﻿using ObsForm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ObsForm.Controllers
+{
+    public class DaneController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpGet]
+        public IActionResult Form() => View();
+
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+                return View("Wynik", dane);
+            else
+                return View(dane);
+        }
+
+        public IActionResult Wynik(Dane dane) => View(dane);
+    }
+}
---
+++ Models/Dane.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ObsForm.Models
+{
+    public class Dane
+    {
+        [Required(ErrorMessage = "Proszę podać imię.")]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać email.")]
+        [EmailAddress(ErrorMessage = "Proszę podać poprawny adres email.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać temat.")]
+        public string Temat { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać treść wiadomości.")]
+        [StringLength(500, MinimumLength = 10, ErrorMessage = "Treść musi mieć od 10 do 500 znaków.")]
+        public string Tresc { get; set; }
+    }
+}
